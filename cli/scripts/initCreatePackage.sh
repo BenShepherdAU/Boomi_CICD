bdc9eb7e43ed1fa7c514ce30979380e250fc3294
BenShepherdAU <90735582+BenShepherdAU@users.noreply.github.com>
2025-12-10 16:29:20 +1100

    Update initCreatePackage.sh

--- cli/scripts/initCreatePackage.sh
+++ cli/scripts/initCreatePackage.sh
@@ -7,6 +7,7 @@ source ${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh
 # Requires:
 # packageVersion, notes, componentType, componentVersion, componentId or processName, extractComponentXmlFolder,tag
 # we will use packageVersion, notes, componentType, componentVersion, componentId, extractComponentXmlFolder
+# usage: "${GITHUB_WORKSPACE}/cli/scripts/initCreatePackage.sh" componentId=$_compId packageVersion=$compVer componentType=$_compType notes="$compNotes" extractComponentXmlFolder="$CodeFolderXML"
 
 ARGUMENTS=(authToken componentId packageVersion notes componentType extractComponentXmlFolder)
 
